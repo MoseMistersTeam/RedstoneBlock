5fa5b26d31f666d343bc97aade37f917b8986235
mosemister <nekronmoseley@gmail.com>
2015-06-27 19:50:25 +0100

    Base Level

--- RedstoneBlock/src/RedstoneBlock/RedstoneBlock.java
+++ RedstoneBlock/src/RedstoneBlock/RedstoneBlock.java
@@ -1,6 +1,28 @@
 package RedstoneBlock;
 
-public class RedstoneBlock {
-	//work in progress
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class RedstoneBlock extends JavaPlugin{
+	//work in progress
+	
+	static RedstoneBlock PLUGIN;
+	
+	public void onEnable(){
+		PLUGIN = this;
+	}
+	
+	public static RedstoneBlock getPlugin(){
+		return PLUGIN;
+	}
+
+	
+	//Test edit by jj
+	// disguise redstone
+	// Have rs + 'tool' in inventory
+	//right clicking a suitable block 'infuses' rs in block
+	//that block now behaves as a piece of redstone in function (can connect to other rs etc)
+	// but retains block texture, break time etc
+	// left-clicking with tool removes redstone inside block
+	// breaking rs infused block yields original block + 1 rs
 }
+
