c958e14b2c9822b45f94881db21fcea791cdd3b0
jobblejosh <joshuapearson096@gmail.com>
2015-06-27 21:19:40 +0100

    Snapshot0.1 (wk1)(dev1)

---
+++ RedstoneBlock/src/RedstoneBlock/Listeners/Listeners.java
@@ -0,0 +1,22 @@
+package RedstoneBlock.Listeners;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class Listeners implements Listener{
+	@EventHandler
+	public static void clickevent(PlayerInteractEvent event){
+		if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+			Player player = event.getPlayer();
+			if(player.getItemInHand().equals(Material.REDSTONE)){
+				ItemStack is = player.getItemInHand();
+				is.setAmount(is.getAmount()-1);
+			}
+		}
+	}
+}
--- RedstoneBlock/src/RedstoneBlock/RedstoneBlock.java
+++ RedstoneBlock/src/RedstoneBlock/RedstoneBlock.java
@@ -27,5 +27,5 @@ public class RedstoneBlock extends JavaPlugin{
 	
 	
 	//test4
+	
 }
-
